61e634198d88c4efb43027dd40e6e272e3c8932a
huangxiaogui <53163988+huangxiaogui@users.noreply.github.com>
2021-06-03 22:02:04 +0800

    dfdfff

--- demo/src/main/java/com/itcast/demo.java
+++ demo/src/main/java/com/itcast/demo.java
@@ -7,5 +7,6 @@ public class demo {
         System.out.println("你好可好我是的人 啊奥迪");
         System.out.println("jiuzheyang");
         System.out.println("我是好人");
+        System.out.println("woxiangni");
     }
 }
