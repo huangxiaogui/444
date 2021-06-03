255573775783512cb0630539dc67cc597a56c4bd
huangxiaogui <53163988+huangxiaogui@users.noreply.github.com>
2021-06-03 21:28:24 +0800

    提交

--- demo/src/main/java/com/itcast/demo.java
+++ demo/src/main/java/com/itcast/demo.java
@@ -2,7 +2,9 @@ package com.itcast;
 
 public class demo {
 
-    public static void main(String[] args) {
-        System.out.println("你好");
+    public static void main(String[] args)
+    {
+        System.out.println("你好不好");
+        System.out.println("我是好人");
     }
 }
