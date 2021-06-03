214b41edda4611981fe9922376e995ea0709859f
huangxiaogui <53163988+huangxiaogui@users.noreply.github.com>
2021-06-03 21:32:02 +0800

    dfdf

--- demo/src/main/java/com/itcast/demo.java
+++ demo/src/main/java/com/itcast/demo.java
@@ -2,9 +2,10 @@ package com.itcast;
 
 public class demo {
 
-    public static void main(String[] args)
-    {
-        System.out.println("你好不好");
+    public static void main(String[] args) {
+
+        System.out.println("你好可好我是的人 啊奥迪");
+        System.out.println("jiuzheyang");
         System.out.println("我是好人");
     }
 }
