db5f397c26e53799c2199fa912c7033a8e591349
huangxiaogui <53163988+huangxiaogui@users.noreply.github.com>
2021-06-03 22:03:56 +0800

    提交

--- demo/src/main/java/com/itcast/demo.java
+++ demo/src/main/java/com/itcast/demo.java
@@ -7,6 +7,7 @@ public class demo {
         System.out.println("你好可好我是的人 啊奥迪");
         System.out.println("jiuzheyang");
         System.out.println("我是好人");
+        System.out.println("kkkkk");
         System.out.println("woxiangni");
     }
 }
